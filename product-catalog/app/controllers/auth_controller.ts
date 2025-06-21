5ecc11d2011cd593884830a60c4f8fdf7b0b4a20
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2025-06-21 21:09:10 +1200

    Update auth_controller.ts
    #1 Show Login

--- product-catalog/app/controllers/auth_controller.ts
+++ product-catalog/app/controllers/auth_controller.ts
@@ -1,4 +1,9 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
 
 export default class AuthController {
+    public async showLogin({ view }: HttpContext) {
+        return view.render('auth/login')
+    }
+
+    
 }
