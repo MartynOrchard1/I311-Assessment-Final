abccb01fc763db0b9ba470f4ffe0d95ee427fb67
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2025-06-22 16:05:42 +1200

    Update 1750563861771_create_products_table.ts

--- product-catalog/database/migrations/1750563861771_create_products_table.ts
+++ product-catalog/database/migrations/1750563861771_create_products_table.ts
@@ -6,9 +6,9 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('title').notNullable()
-      table.text('description').nullable()
+      table.string('name').notNullable()
       table.decimal('price', 10, 2).notNullable()
+      table.text('description').nullable()
       table.string('image_url').nullable()
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
